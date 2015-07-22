abe7674feb4c1a6d0db1174f689f4a7996b68e9a
Daniel Stankevich <standeo@gmail.com>
2015-07-22 10:42:16 +1000

    NICTA Day 2 - Functor instances

--- src/Course/List.hs
+++ src/Course/List.hs
@@ -72,9 +72,9 @@ foldLeft f b (h :. t) = let b' = f b h in b' `seq` foldLeft f b' t
 --
 -- prop> x `headOr` Nil == x
 headOr :: a -> List a -> a
-{-headOr x Nil = x-}
-{-headOr _ (h :. _) = h-}
-headOr = foldRight const
+headOr x Nil = x
+headOr _ (h :. _) = h
+{-headOr = foldRight const-}
 
 -- | The product of the elements of a list.
 --
